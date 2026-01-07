8fc96fab0a833f6798743874cbfb9d8aa6a69e0e
aleckshen <aleckshn@gmail.com>
2026-01-07 13:09:57 +1300

    feat: added mini-nvim plugins

---
+++ nvim/lua/plugins/mini-nvim.lua
@@ -0,0 +1,6 @@
+return {
+    { 'nvim-mini/mini.comment', version = '*', opts = {} }, -- comment/uncomment lines
+    { 'nvim-mini/mini.surround', version = '*', opts = {} }, -- surround a word with characters
+    { 'nvim-mini/mini.indentscope', version = '*', opts = {} }, -- visualize scope with animated vertical line
+    { 'nvim-mini/mini.notify', version = '*', opts = {} }, -- shows notification in floating window
+}
