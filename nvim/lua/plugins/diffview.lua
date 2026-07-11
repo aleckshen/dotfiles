b9b00f8bc6a4f91b3cd6a513b6c58260b5cf7303
aleckshen <aleckshn@gmail.com>
2026-07-12 00:39:32 +1200

    feat(nvim): add diffview.nvim

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,13 @@
+return {
+	"sindrets/diffview.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	cmd = {
+		"DiffviewOpen",
+		"DiffviewClose",
+		"DiffviewFileHistory",
+		"DiffviewFocusFiles",
+		"DiffviewToggleFiles",
+		"DiffviewRefresh",
+	},
+	opts = {},
+}
